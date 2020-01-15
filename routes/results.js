ac07d32548df9357ed5ade25460449c22928162f
pyrrhos-msits <menzelf@mediamarktsaturn.com>
2020-01-15 17:43:38 +0100

    Berechnung Ergebnis erster Versuch

--- routes/results.js
+++ routes/results.js
@@ -1,9 +1,36 @@
 const express = require('express');
 const router = express.Router();
+const Results = require('../models/results');
 
-router.get('/results', (req, res, next) => {
-  res.render('results');
+
+
+router.get('/results', (req, res) => {
+  Results.findById('5e1f1a6b0170a12fac7eced5')
+   .then(result => {
+    console.log(result.userName);
+    let scores = result.score;
+    let questions = result.numberOfCases;
+    console.log("scores are : " + scores + "and Number of Cases are " + questions);
+    let ergebnis = (scores / questions) * 100
+    console.log("Dein Ergebnis : "+ ergebnis + " Prozent");
+     res.render('results', {result});
+     
+   })
+   .catch(err => {
+     next(err);
+   })
+  });
+  
+
+
+/*
+router.get('/new', (req, res) => {
+    Celebrity.find().then(celebrities => {
+        res.render('movies/new', { celebrities });
+    })
+        .catch(err => {
+            next(err);
+        });
 });
-
-
+*/
 module.exports = router;
