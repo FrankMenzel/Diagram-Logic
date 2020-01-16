9d5d149e84a355b5975873149a40b03480dbcb21
pyrrhos-msits <menzelf@mediamarktsaturn.com>
2020-01-16 10:29:34 +0100

    Last Stand after delete console logs from resultRoute

--- routes/results.js
+++ routes/results.js
@@ -7,14 +7,11 @@ const Results = require('../models/results');
 router.get('/results', (req, res) => {
   Results.findById('5e1f1a6b0170a12fac7eced5')
    .then(result => {
-    console.log(result.userName);
     let scores = result.score;
     let questions = result.numberOfCases;
-    console.log("scores are : " + scores + "and Number of Cases are " + questions);
     let ergebnis = (scores / questions) * 100
     console.log("Dein Ergebnis : "+ ergebnis + " Prozent");
-     res.render('results', {result});
-     
+    res.render('results', {result});
    })
    .catch(err => {
      next(err);
@@ -22,15 +19,4 @@ router.get('/results', (req, res) => {
   });
   
 
-
-/*
-router.get('/new', (req, res) => {
-    Celebrity.find().then(celebrities => {
-        res.render('movies/new', { celebrities });
-    })
-        .catch(err => {
-            next(err);
-        });
-});
-*/
 module.exports = router;
