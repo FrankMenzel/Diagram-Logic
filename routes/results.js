a065bbaab6ef55031c6449c190524f11c486b107
pyrrhos-msits <56028011+pyrrhos-msits@users.noreply.github.com>
2020-01-20 14:15:13 +0100

    Ordering

--- routes/results.js
+++ routes/results.js
@@ -10,6 +10,7 @@ router.get('/results', (req, res) => {
   Results.findOne({"userName" : sessUs}).sort({createdAt: 'desc'}).limit(1)
    .then(userResult => {
     let scores = userResult.score;
+    
     let questions = userResult.numberOfCases;
     let ergebnis = (scores / questions) * 100
     console.log("Dein Ergebnis : "+ ergebnis + " Prozent");
