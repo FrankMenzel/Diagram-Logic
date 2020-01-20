b9d6bc93e8ff6d104842ab70d6de3566ddaf6d95
Bialas <bialaska@media-saturn.com>
2020-01-20 16:40:17 +0100

    style correction

--- routes/privat.js
+++ routes/privat.js
@@ -2,6 +2,10 @@ const express = require('express');
 const router  = express.Router();
 
 router.get('/privat', (req, res, next) => {
+  if (!req.session.user){
+    res.redirect('/login');
+    return;
+  }
   res.render('privatpage');
 });
 
